44dad5a8e3ca47b815f68be5fbc3c317faf9f9b8
sulaiman1808 <55393597+sulaiman1808@users.noreply.github.com>
2019-11-05 20:37:23 +0500

    Searches For A Particular Character in An Array And Displays Its Position

---
+++ Linear Search.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim Number(8) As String
+        Dim i As Integer = 0
+        Dim IsFound As Boolean = False
+        Dim X As String = ""
+        Dim Y As String = ""
+
+        For i = 1 To 8
+            Console.Write("Enter Values For An Array: ")
+            X = Console.ReadLine
+            Number(i) = X
+        Next
+
+        Console.Write("Enter The Value You Want To Look For: ")
+        Y = Console.ReadLine
+
+        For i = 1 To 8
+            If Number(i) = Y Then
+                IsFound = True
+                Exit For
+            End If
+        Next
+
+    
+
+        If IsFound = True Then
+            Console.WriteLine("Found At " & i)
+        ElseIf IsFound = False Then
+            Console.WriteLine("Not Found")
+        End If
+
+        Console.ReadKey()
+
+    End Sub
+
+End Module
