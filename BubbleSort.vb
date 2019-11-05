54b1a2357c222594171538ba60204e4f49a4cc4e
sulaiman1808 <55393597+sulaiman1808@users.noreply.github.com>
2019-11-05 19:48:48 +0500

    BubbleSort is a simple sorting algorithm that compares adjacent elements and swaps them if they are in the wrong order.

---
+++ BubbleSort.vb
@@ -0,0 +1,36 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim Number(8) As Integer
+        Dim I As Integer = 0
+        Dim a As Integer = 0
+        Dim b As Integer = 0
+        Dim X As Integer = 0
+
+        For I = 1 To 8
+            Console.Write("Enter Values For An Array: ")
+            a = Console.ReadLine
+            Number(I) = a
+
+        Next
+
+        For C = 7 To 1 Step -1
+            For I = 1 To C
+                If Number(I) > Number(I + 1) Then
+                    X = Number(I)
+                    Number(I) = Number(I + 1)
+                    Number(I + 1) = X
+
+                End If
+            Next
+        Next
+        Console.WriteLine("The Array's Values Rearranged In Ascending Order Are: ")
+        For I = 1 To 8
+            b = Number(I)
+            Console.WriteLine(b)
+        Next
+        Console.ReadKey()
+
+    End Sub
+
+End Module
