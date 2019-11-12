57febfb551aea48687c486d56bf4eec1983d2c56
sulaiman1808 <55393597+sulaiman1808@users.noreply.github.com>
2019-11-12 20:46:55 +0500

    Populates A Single-Dimensional Array With Unique Random Numbers

---
+++ Unique Random Number Generation In A 1D Array.vb
@@ -0,0 +1,39 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Dim NoArr(10) As Integer
+        Dim A As Integer = 0
+        Dim B As Integer = 0
+        Dim C As Integer = 0
+        Dim D As Integer = 0
+        Dim AlreadyFound As Boolean = False
+
+        Randomize()
+
+
+        For A = 1 To 10
+            NoArr(A) = 0
+        Next
+
+        For A = 1 To 10
+            Do
+                C = C + 1
+                AlreadyFound = False
+                B = Int(Rnd() * 20) + 1
+                For D = A To 1 Step -1
+                    If NoArr(D) = B Then AlreadyFound = True
+                Next
+            Loop Until AlreadyFound = False
+            NoArr(A) = B
+        Next
+
+        For A = 1 To 10
+            Console.WriteLine(NoArr(A))
+        Next
+
+        Console.WriteLine("LOOP WORKED FOR " & C & " TIMES")
+        Console.ReadKey()
+    End Sub
+
+End Module
